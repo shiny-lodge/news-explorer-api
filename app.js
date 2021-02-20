cd405a2ba2d0c61b1477eaf83a08644638684ccd
shiny-lodge <lomovitskiy-d@ya.ru>
2021-02-20 16:34:24 +0300

    cors fix, cookie parser added

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 require('dotenv').config();
 
+const cookieParser = requier('cookie-parser');
 const express = require('express');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
 const router = require('./routes/index');
 const { dbOptions, dbUrl, port } = require('./utils');
-
+const cors = require('cors');
 const middlewaresRouter = require('./middlewares');
 const { errorsHandler } = require('./middlewares/errors-handler');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
@@ -16,7 +17,20 @@ mongoose.connect(dbUrl, dbOptions);
 
 app.use(middlewaresRouter);
 
+app.use(cookieParser());
+
 app.use(requestLogger);
+
+app.use(cors({
+    origin: [
+      'http://localhost:3000',
+      'http://www.lomovitsky.ru',
+      'https://www.lomovitsky.ru',
+      'http://lomovitsky.ru',
+      'https://lomovitsky.ru',
+    ],
+    credentials: true,
+  }));
 
 app.use(router);
 
